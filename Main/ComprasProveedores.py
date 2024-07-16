ce19a9b810f927182a2a1a50f60f86ad0af095c0
diegoOsorioB <osoriodiego151@gmail.com>
2024-07-16 17:47:38 -0600

    Compras terinadas 80%

--- Main/ComprasProveedores.py
+++ Main/ComprasProveedores.py
@@ -11,21 +11,30 @@ def pedidoProveedor():
         if opcion == "1":
             Proveedores.mostrar()
             nombreProveedor = input("Ingrese el nombre del proveedor: ")
+            while not nombreProveedor:
+                nombreProveedor = input("Ingrese el nombre del proveedor: ")
             proveedor = Proveedores.validar_provedor(nombreProveedor)
             print(proveedor)
             if proveedor:
                 nombreProducto = input("Ingrese el codigo del producto: ")
+                while not nombreProducto:
+                    nombreProducto = input("Ingrese el codigo del producto registrado: ")
                 prodcutoN=Producto.validar_codigo(nombreProducto)
                 if prodcutoN:
                     marcaProducto = input("Ingrese la marca del producto: ")
+                    while not marcaProducto:
+                        marcaProducto = input("Ingrese la marca del producto registrado: ")
                     prodcutoM=Producto.validar_marca(marcaProducto)
                     if prodcutoM:
-                        cantidadProducto = int(input("Ingrese la cantidad de productos: "))
-                        if cantidadProducto>0:
-                           print(nombreProveedor)
-                           guarda= PedidosProveedor(nombreProveedor,nombreProducto,marcaProducto,cantidadProducto)
-                           guarda.guardar()
-
+                        cantidadProducto = (input("Ingrese la cantidad de productos: "))
+                        if cantidadProducto.isdigit():
+                            if int(cantidadProducto) > 0:
+                                guarda = PedidosProveedor(nombreProveedor, nombreProducto, marcaProducto,cantidadProducto)
+                                guarda.guardar()
+                            else:
+                                print("No se pueden pedir menos de 0  cosas")
+                        else:
+                            print("Cantidad de productos invalido")
                     else:
                         print("Marca de producto no registrada")
                 else:
@@ -48,30 +57,45 @@ def menuComprasProveedor():
         print("1. Realizar pedido de compra")
         print("2. Validacion de entregas")
         print("3. Registrar devoluciones")
-        print("4. Generar historiles")
+        print("4. Generar Historiales de compra")
         print("5. Salir")
         opcion = input("Ingrese una opcion: ")
         if opcion == "1":
             pedidoProveedor()
         elif opcion == "2":
-            print("2. Validacion de entregas")
+            try:
+                id = input("Ingrese el id del pedido: ")
+                cantidad = int(input("Ingrese la cantidad de productos: "))
+                PedidosProveedor.pedidos_proveedorID(id, cantidad)
+            except ValueError as e:
+                print("Ingrese datos correctos")
         elif opcion == "3":
-            print("Registrar devoluciones")
+            print("DESCUENTA EL VALOR QUE SE INTRODUCE AL INVENTARIO Y LISTO")
+            productos = input("Ingrese el producto para la devolucion: ")
+            cantidad = int(input("Ingrese la cantidad de productos: "))
+
         elif opcion == "4":
             while True:
-                print("----Menu historiales----")
+                print("----Menu Historiales----")
                 print("1. Historial de compras")
                 print("2. Historial de compras por proveedor")
                 print("3. salir..")
                 opcion2 = input("Ingrese una opcion: ")
                 if opcion2 == "1":
-                    print("1. Historial de compras")
                     PedidosProveedor.mostrar_pedidos()
-                elif opcion == "2":
+                elif opcion2 == "2":
                     print("2. Historial de compras por proveedor")
+                    nombreProveedor = input("Ingrese el nombre del proveedor: ")
+                    proveedor = Proveedores.validar_provedor(nombreProveedor)
+                    if proveedor:
+                        PedidosProveedor.pedidos_proveedor(nombreProveedor)
+                    else:
+                        print("Nombre de proveedor no registrado")
                 elif opcion2 == "3":
                     print("Saliendo...")
                     break
+                else:
+                    print("Opcion no valida")
         elif opcion == "5":
             print("Saliendo...")
             break
--- Main/PedidosProveedor.py
+++ Main/PedidosProveedor.py
@@ -1,8 +1,11 @@
 
 class PedidosProveedor:
+    idAuto=0
     pedidos = []
 
     def __init__(self, proveedor, nombre, marca, cantidad):
+        PedidosProveedor.idAuto += 1
+        self.id = PedidosProveedor.idAuto
         self.proveedor = proveedor
         self.nombre = nombre
         self.marca = marca
@@ -18,5 +21,36 @@ class PedidosProveedor:
         else:
             for pedido in cls.pedidos:
                 print(
-                    f"Proveedor: {pedido.proveedor}, Nombre: {pedido.nombre}, Marca: {pedido.marca}, Cantidad: {pedido.cantidad}")
+                    f"ID: {pedido.id}, Proveedor: {pedido.proveedor}, Nombre: {pedido.nombre}, Marca: {pedido.marca}, Cantidad: {pedido.cantidad}")
+
+    @classmethod
+    def pedidos_proveedor(self,proveedor):
+        if not self.pedidos:
+            print("No hay pedidos guardados con este proveedor.")
+        else:
+            for pedido in self.pedidos:
+                if pedido.proveedor == proveedor:
+                    print(
+                        f"ID: {pedido.id}, Proveedor: {pedido.proveedor}, Nombre: {pedido.nombre}, Marca: {pedido.marca}, Cantidad: {pedido.cantidad}")
+                else:
+                    print("Pedido no encontrado")
+
+    @classmethod
+    def pedidos_proveedorID(self, i,cantidad):
+        if not self.pedidos:
+            print("No hay pedidos guardados con este proveedor.")
+        else:
+            for pedido in self.pedidos:
+                if pedido.nombre == i:
+                    print(
+                        f"ID: {pedido.id}, Proveedor: {pedido.proveedor}, Nombre: {pedido.nombre}, Marca: {pedido.marca}, Cantidad: {pedido.cantidad}")
+                    if int(pedido.cantidad) < int(cantidad):
+                        print("Se exedio la cantidad de producto")
+                    elif int(pedido.cantidad) > int(cantidad):
+                        print("Entrega Incompleta")
+                    else:
+                        print("Entrega correcta")
+                else:
+                    print("Pedido no encontrado")
+
 
