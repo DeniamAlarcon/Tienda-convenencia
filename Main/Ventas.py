b9ea9a076f60cb4687eea56da590ff3e7e29db98
DeniamAlarcon <deniamalarcon@gmail.com>
2024-07-17 08:09:52 -0600

    Parte de ventas

--- Main/Ventas.py
+++ Main/Ventas.py
@@ -1,26 +1,42 @@
-from Proveedores import *
-from Productos import *
-from Inventario import *
 
+from Inventario import *
+from VentasMain import *
+from Productos import *
 
 def acceder_apartado_ventas():
     print("Accediendo al apartado de ventas")
 
 
 def buscar_producto():
-    producto = input("Ingrese el nombre del producto: ")
-    print(f"Buscando el producto '{producto}'...")
-    return producto
+    producto = ""
+    while not producto:
+        producto = input("Ingrese el nombre del producto: ")
+        print(f"Buscando el producto '{producto}'...")
+        for i in Producto.lista_productos:
+            if i.nombre == producto:
+                return producto
+            else:
+                print("Producto no encontrado")
+                producto = ""
 
 
 def agregar_producto_a_venta(producto):
     print(f"Agregando '{producto}' a la venta...")
 
 
-def seleccionar_cantidad_producto():
-    cantidad = int(input("Ingrese la cantidad del producto: "))
-    print(f"Seleccionando {cantidad} unidades...")
-    return cantidad
+def seleccionar_cantidad_producto(producto):
+    cantidad = input("Ingrese la cantidad del producto: ")
+    for i in Producto.lista_productos:
+        if i.nombre == producto:
+            if int(cantidad) > int(i.stock):
+                print("La cantidad excede el stock")
+                cantidad = ""
+            elif int(cantidad) == 0:
+                print("La cantidad debe ser mayor a 0")
+                cantidad = ""
+            else:
+                print(f"Seleccionando {cantidad} unidades...")
+                return cantidad
 
 
 def seleccionar_metodo_pago():
@@ -36,21 +52,32 @@ def verificar_pago(monto_venta):
     else:
         print("Dinero insuficiente")
 
+def cantidad_pagar(venta_actual):
+    incremental = 0
+    for i in venta_actual:
+        incremental += 1
+        print(f"{incremental} {i.nombre} {i.cantidad} {VentasMain.total_venta_actual(i.nombre,i.cantidad)}")
+
 
 def menuVentas():
     if Proveedores.proveedores:
         if Producto.lista_productos:
             acceder_apartado_ventas()
-            n = int(input("¿Cuántos productos desea buscar? "))
+            n = int(input("¿Cuántos productos desea vender? "))
             productos = []
+            venta_actual=[]
             for i in range(n):
                 producto = buscar_producto()
                 agregar_producto_a_venta(producto)
-                cantidad = seleccionar_cantidad_producto()
+                cantidad = seleccionar_cantidad_producto(producto)
+                venta = VentasMain(cantidad,producto)
+                venta.guardar_venta()
+                venta_actual=venta.venta_actual()
                 productos.append((producto, cantidad))
             metodo_pago = seleccionar_metodo_pago()
-
+            cantidad_pagar(venta_actual)
             monto_venta = sum(cantidad for _, cantidad in productos)  # Asume que el precio de cada producto es 1 unidad
+
             print(f"El monto total de la venta es: {monto_venta}")
             verificar_pago(monto_venta)
         else:
